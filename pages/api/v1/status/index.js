9bdf16ea0ea9bcc18e05ac9d026e535fdb826b07
Elias Souza <eliasnsouza1245@gmail.com>
2023-11-29 20:17:29 +0000

    add `database.js` and `.env.development` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,8 @@
-export default function status(request, response) {
+import database from "../../../../infra/database.js";
+
+export default async function status(request, response) {
+  const result = await database.query("SELECT 1+1 as result;");
+
+  console.log(result.rows[0]);
   response.status(200).json({ hello: "world" });
 }
