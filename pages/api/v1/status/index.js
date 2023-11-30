70e87086c418c13063d7165b59f2a87843410c10
Elias Souza <eliasnsouza1245@gmail.com>
2023-11-30 20:42:30 +0000

    add health info to `api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,64 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as result;");
+  const updatedAt = new Date().toISOString();
 
-  console.log(result.rows[0]);
-  response.status(200).json({ hello: "world" });
+  async function checkDatabaseHealth() {
+    let health;
+
+    try {
+      const firstTimerInit = performance.now();
+      const [maxConnectionsResult, superuserReservedConnectionsResult] =
+        await database.query(
+          "SHOW max_connections; SHOW superuser_reserved_connections",
+        );
+      const maxConnectionsValue = maxConnectionsResult.rows[0].max_connections;
+      const superuserReservedConnectionsValue =
+        superuserReservedConnectionsResult.rows[0]
+          .superuser_reserved_connections;
+      const firstQueryDuration = performance.now() - firstTimerInit;
+
+      const secondTimerInit = performance.now();
+      const openedConnectionsResult = await database.query(
+        "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db'",
+      );
+      const openedConnectionsValue =
+        openedConnectionsResult.rows[0].count;
+      const secondQueryDuration = performance.now() - secondTimerInit;
+
+      const thirdTimerInit = performance.now();
+      const versionResult = await database.query("SHOW server_version");
+      const versionValue = versionResult.rows[0].server_version;
+      const thirdQueryDuration = performance.now() - thirdTimerInit;
+
+      health = {
+        status: "healthy",
+        version: versionValue,
+        max_connections:
+          parseInt(maxConnectionsValue),
+        opened_connections: openedConnectionsValue,
+        latency: {
+          first_query: firstQueryDuration,
+          second_query: secondQueryDuration,
+          third_query: thirdQueryDuration,
+        },
+      };
+    } catch (error) {
+      console.log(error);
+      health = {
+        status: "unhealthy",
+      };
+    }
+
+    return health;
+  }
+
+  const result = {
+    updated_at: updatedAt,
+    dependencies: {
+      database: await checkDatabaseHealth(),
+    },
+  };
+
+  response.status(200).json(result);
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,18 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const data = await response.json();
+  expect(data.updated_at).toBeDefined();
+
+  const parsedDate = new Date(data.updated_at).toISOString();
+  expect(parsedDate).toBe(data.updated_at);
+
+  expect(data.dependencies.database.status).toBe("healthy");
+  expect(data.dependencies.database.version).toEqual("16.0");
+  expect(data.dependencies.database.max_connections).toEqual(100);
+  expect(data.dependencies.database.opened_connections).toEqual(1);
+  expect(typeof data.dependencies.database.latency.first_query).toBe("number");
+  expect(typeof data.dependencies.database.latency.second_query).toBe("number");
+  expect(typeof data.dependencies.database.latency.third_query).toBe("number");
 });
