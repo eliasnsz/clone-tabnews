a304b2abb560c768d565cb187805e91fb5ec1a5a
Elias Souza <eliasnsz.dev@gmail.com>
2025-04-22 19:52:31 -0300

    makes `npm test` more robust with `async-retry` and `orchestrator.js`

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,13 @@ const nextJest = require("next/jest");
 const dotenv = require("dotenv");
 
 dotenv.config({
-	path: ".env.development",
+  path: ".env.development",
 });
 
 const createJestConfig = nextJest({ dir: "./" });
 const jestConfig = createJestConfig({
-	moduleDirectories: ["node_modules", "<rootDir>"],
+  moduleDirectories: ["node_modules", "<rootDir>"],
+  testTimeout: 1000 * 60, // 60s
 });
 
 module.exports = jestConfig;
